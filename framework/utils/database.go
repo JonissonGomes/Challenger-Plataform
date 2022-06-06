438df39845ff87f60f16c690604fa3ee2bcc333d
JonissonGomes <joni_gomes@hotmail.com>
2022-06-05 23:58:52 -0300

    Criando função para a conexão com o banco de dados

---
+++ framework/utils/database.go
@@ -0,0 +1,36 @@
+package utils
+
+import (
+	"log"
+	"os"
+
+	"github.com/JonissonGomes/Challenger-Plataform/domain"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+)
+
+func ConnectDatabase() *gorm.DB {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Lê o arquivo no .env
+	readConnectionDatabase := os.Getenv("connectionDB")
+
+	// Conecta com o banco
+	connectDatabase, err := gorm.Open("postgres", readConnectionDatabase)
+
+	// Valida se a conexão foi estabelecida
+	if err != nil {
+		log.Fatalf("Error connecting to database : %v", err)
+		panic(err)
+	}
+
+	// Cria as tabelas automaticamente, baseado na estrutura passada como parametro
+	connectDatabase.AutoMigrate(&domain.User{})
+
+	return connectDatabase
+}
