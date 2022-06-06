ecdf9e57cba277ddb5ef5cdbe95635c8e2743851
JonissonGomes <joni_gomes@hotmail.com>
2022-06-05 23:29:12 -0300

    Construindo base da função para criptografia e validação dos dados

--- domain/user.go
+++ domain/user.go
@@ -1,9 +1,33 @@
 package domain
 
+import (
+	"log"
+
+	uuid "github.com/stori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	BaseStructModel
 	Name     string
 	Email    string
 	Password string
 	TokenId  string
+}
+
+func (user *User) Prepare() error {
+
+	// Encryptando senha
+	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+	// Tratando erros
+	if err != nil {
+		log.Fatalf("Error in duration password generation: %v", err)
+	}
+
+	// Atribuindo senha encriptada
+	user.Password = string(password)
+
+	// Gerando Token automático
+	user.TokenId = uuid.NewV4().String()
 }
