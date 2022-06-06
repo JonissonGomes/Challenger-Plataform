320dba25fb513cf855f59bd5790d090e4b845cf5
JonissonGomes <joni_gomes@hotmail.com>
2022-06-06 00:57:43 -0300

    Ajustando struct para integração com o postgres

--- domain/utils.go
+++ domain/utils.go
@@ -9,10 +9,10 @@ import (
 )
 
 type BaseStructModel struct {
-	ID        string     `json:"id" gorm:"type:uuid; primary_key"`
-	CreatedAt time.Timer `json:"created_at" gorm:"type:datetime"`
-	UpdateAt  time.Timer `json:"updated_at" gorm:"type:datetime"`
-	DeletedAt time.Timer `json:"deleted_at" gorm:"type:datetime" sql:"index"`
+	ID        string    `json:"id" gorm:"type:uuid; primary_key"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdateAt  time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at" sql:"index"`
 }
 
 func CreateNewUser() *User {
