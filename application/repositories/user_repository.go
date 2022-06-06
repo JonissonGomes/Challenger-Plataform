567c0447a7a121a9cd4623d0fa08268768516f7d
JonissonGomes <joni_gomes@hotmail.com>
2022-06-06 00:41:54 -0300

    Criando repositório de usuários

--- application/repositories/user_repository.go
+++ application/repositories/user_repository.go
@@ -1,7 +1,36 @@
 package repositories
 
-import "github.com/JonissonGomes/Challenger-Plataform/domain"
+import (
+	"log"
+
+	"github.com/JonissonGomes/Challenger-Plataform/domain"
+	"github.com/jinzhu/gorm"
+)
 
 type UserRepository interface {
 	Insert(user *domain.User) (*domain.User, error)
+}
+
+// Struct para armazenar os dados no banco
+type UserRepositoryDataBase struct {
+	Db *gorm.DB
+}
+
+func (repo UserRepositoryDataBase) Insert(user *domain.User) (*domain.User, error) {
+	err := user.Prepare()
+
+	if err != nil {
+		log.Fatalf("Error during the user validation: %v", err)
+	}
+
+	// Persistindo dados no banco
+	err = repo.Db.Create(user).Error
+
+	if err != nil {
+		log.Fatalf("Error to persist user on database: %v", err)
+		return user, err
+	}
+
+	// Retornando resultado da operação
+	return user, nil
 }
