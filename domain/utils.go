89e9d4dac39a3b41f62978cd6800979933cfc86d
JonissonGomes <joni_gomes@hotmail.com>
2022-06-06 00:13:04 -0300

    Criando função para aplicar fluxo padrão a inicialização da estrutura

--- domain/utils.go
+++ domain/utils.go
@@ -1,10 +1,32 @@
 package domain
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+)
 
 type BaseStructModel struct {
 	ID        string     `json:"id" gorm:"type:uuid; primary_key"`
 	CreatedAt time.Timer `json:"created_at" gorm:"type:datetime"`
 	UpdateAt  time.Timer `json:"updated_at" gorm:"type:datetime"`
 	DeletedAt time.Timer `json:"deleted_at" gorm:"type:datetime" sql:"index"`
+}
+
+func (base *BaseStructModel) BeforCreate(scope *gorm.Scope) error {
+	err := scope.SetColumn("ID", uuid.NewV4().String())
+
+	if err != nil {
+		log.Fatalf("Erro during create object")
+	}
+
+	err = scope.SetColumn("CreatedAt", time.Now())
+
+	if err != nil {
+		log.Fatalf("Erro during create object")
+	}
+
+	return nil
 }
